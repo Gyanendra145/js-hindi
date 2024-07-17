8452656f67e17ad81621fc0c5952c275b0f925c2
Gyanendra145 <116753180+Gyanendra145@users.noreply.github.com>
2024-07-17 12:56:43 +0000

    objects_02

--- 02_array/object_2.js
+++ 02_array/object_2.js
@@ -23,6 +23,11 @@ const obj1 = {1: "a",2:"b"}
 const obj2 = {2: "a",4:"b"}
 const obj3 = {3: "a",5:"b"}
 
-const obj4 = Object.assign({}, obj1,obj2,obj3)
-console.log(obj4);
+// const obj4 = Object.assign({}, obj1,obj2,obj3)
+const obj4 = {...obj1,...obj2,...obj3}
+// console.log(obj4);
 
+users[1].email
+console.log(tinderUser);
+
+console.log();
