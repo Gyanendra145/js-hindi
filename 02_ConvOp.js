35d0986ebd8f2e2957eb7b2d02a181927fa27a1e
Gyanendra145 <116753180+Gyanendra145@users.noreply.github.com>
2024-07-17 07:49:25 +0000

    Maths and nums

--- 02_ConvOp.js
+++ 02_ConvOp.js
@@ -1,19 +1,19 @@
-// let score = true
+let score = true
 
-// console.log(typeof (score));
-// let valueInNumber = Number(score)
-// console.log(typeof (valueInNumber));
-// console.log(valueInNumber);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
 
-// let isLoggedIn = "dfg"
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+let isLoggedIn = "dfg"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 
-// let someNum = 34
+let someNum = 34
 
-// let StringNum = String(someNum)
-// console.log(StringNum);
-// console.log(typeof StringNum);
+let StringNum = String(someNum)
+console.log(StringNum);
+console.log(typeof StringNum);
 
 let value = 45
 let negvalue=  -value
--- maths.js
+++ maths.js
@@ -1,9 +1,22 @@
-const score = 4000
+// const score = 4000
 
-const balance = new Number(200)
+// const balance = new Number(200)
 
-console.log(balance);
+// console.log(balance);
 
-console.log(score.toString().length);
+// console.log(score.toString().length);
 
-console.log(score.toFixed(1));
+// console.log(score.toFixed(1));
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+
+// console.log(Math.max(2,3,4,5));
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
