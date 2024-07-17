a8843861dc9205e4fba43ca781f0ec501f340e1a
Gyanendra145 <116753180+Gyanendra145@users.noreply.github.com>
2024-07-17 08:43:12 +0000

    Dates & Array

---
+++ 02_array/array.js
@@ -0,0 +1,1 @@
+console.log(Math.max(2,3,4,5,6,7,8,9,0));
---
+++ dates.js
@@ -0,0 +1,21 @@
+// let myDate = new Date()
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024 , 0 ,24)
+
+// console.log(myCreatedDate.toDateString());
+let myCreatedDate = new Date("2024-01-14")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
+
