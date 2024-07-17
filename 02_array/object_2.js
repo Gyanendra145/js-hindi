3339916fd5d463e0d4cde38854c45ac810fb0268
Gyanendra145 <116753180+Gyanendra145@users.noreply.github.com>
2024-07-17 12:32:26 +0000

    Object

---
+++ 02_array/object_2.js
@@ -0,0 +1,28 @@
+// const tinderuser = new Object() -- Singleton user
+
+const tinderUser = {}
+
+tinderUser.id = "12"
+tinderUser.name = "GYAN_SINGH"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "Some@gm.com",
+    fullname:{
+        userfullname : {
+            firstname : "GYAN",
+            lastname: "singh"
+
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+const obj1 = {1: "a",2:"b"}
+const obj2 = {2: "a",4:"b"}
+const obj3 = {3: "a",5:"b"}
+
+const obj4 = Object.assign({}, obj1,obj2,obj3)
+console.log(obj4);
+
